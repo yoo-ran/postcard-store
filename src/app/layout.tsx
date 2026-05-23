f208caaab64cda4beabcc73af863982bc1549a7a
YooranKim <yuranm@naver.com>
2026-05-22 20:36:23 -0700

    feat(auth): wrap root layout with SessionProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from '@/components/layout/Navbar';
 import './globals.css';
 import Footer from '@/components/layout/Footer';
+import Providers from '@/components/Provider';
 
 export default function RootLayout({
   children,
@@ -10,9 +11,11 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body suppressHydrationWarning>
-        <Navbar />
-        <main className='mx-auto max-w-6xl px-4 py-8'>{children}</main>
-        <Footer />
+        <Providers>
+          <Navbar />
+          <main className='mx-auto max-w-6xl px-4 py-8'>{children}</main>
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
