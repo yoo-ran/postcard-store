b227d7f9b205783cee3674d7ff828ff11765270a
YooranKim <yuranm@naver.com>
2026-06-16 22:37:56 -0700

    fix(build): generate Prisma Client without engine for Vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
-import { withAccelerate } from '@prisma/extension-accelerate';
 import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
 const prismaClientSingleton = () => {
   return new PrismaClient().$extends(withAccelerate());
